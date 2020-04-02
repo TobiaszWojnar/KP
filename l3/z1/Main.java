0cebfdfd40efc80fa5cf252642ffbfd9a35ae2c1
Tobiasz <wojnar.tobiasz@gmail.com>
2020-04-02 16:12:27 +0200

    created classes for l3z1

---
+++ l3/z1/Main.java
@@ -0,0 +1,39 @@
+/**
+ * @author Tobiasz Wojnar
+ * Java Programing Course 2020
+ * list 3 exercise 1
+ * Calculate surface area and circumference of:
+ * circle, quadrangle (square, rectangle, rhombus), regular pentagon, regular hexagon
+ * implement hierarchy and abstract class Figure 
+ * handle exeptions and bad data
+ *
+ * @input first letter of figure and paramethers 
+ *	for circle			- radious
+ *	for quadrangle			- 4 sides and angle
+ *	for pentagon and hexagon	- side
+ * eg.: java figures q 8 8 4 4 90 //for now its going to be java main q 8 8 4 4 90
+ **/
+
+public class Main {
+    public static void main (String[] liczba){
+        int i;
+        for (String s : liczba) {
+            try {
+                i = Integer.parseInt(s);
+                System.out.println(RzymArab.arab2rzym(i));
+            }
+            catch (RzymArabException k){
+                System.out.println(k.getMessage());
+            }
+            catch (NumberFormatException d) {
+                try {
+                    i = RzymArab.rzym2arab(s);
+                    System.out.println(i);
+                }
+                catch (RzymArabException k) {
+                    System.out.println(k.getMessage());
+                }
+            }
+        }
+    }
+}
