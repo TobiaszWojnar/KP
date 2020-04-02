68c70da73ba972d1d74cb5023517bbaf149b21b9
Tobiasz <wojnar.tobiasz@gmail.com>
2020-04-02 16:09:10 +0200

    fixed when starting program with no paramethers

--- l1/z1/PrimeNumbers.java
+++ l1/z1/PrimeNumbers.java
@@ -15,23 +15,23 @@ public class PrimeNumbers {
     public PrimeNumbers (int n) {
 
         boolean[] tabbool = new boolean [n+1];
-        for (int i = 0; i <= n; i++)                  //initialise
+        for (int i = 0; i <= n; i++)                  //initialise a table for finding prime numbers
             tabbool[i] = false;
 
-        int counter = 0;
+        int counter = 0;                              //counts how many prime numbers did we find
         for(int i = 2; i<=n; i++) {                   //Sieve of Eratosthenes
-            if (! tabbool [i]){
+            if (! tabbool [i]){                       //if a number has not been changed to true it is prime
                 counter ++;
-                for (int j = i+i; j <= n; j += i) {
+                for (int j = i+i; j <= n; j += i) {   //in that case we mark every multiple of this number as true
                     tabbool [j] = true;
                 }
             }
         }
-        tab = new int [counter];
+        tab = new int [counter];                      //a new table has only primes
         int j = 0;
-        for(int i = 2; i<=n; i++) {                   //Sieve of Eratosthenes
-            if (! tabbool [i]){
-                tab [j]=i;
+        for(int i = 2; i<=n; i++) {
+            if (! tabbool [i]){                       //if the number has not been changed to true it is prime
+                tab [j]=i;                            //we put it in a new table
                 j++;
             }
         }
--- l1/z1/Test.java
+++ l1/z1/Test.java
@@ -10,37 +10,37 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        int n;
-        try {
-            n = Integer.parseInt(args[0]);
-        } catch (NumberFormatException ex) {
-            System.out.println(args[0] + " - not integer");
-            return;
-        }
-        if (n < 1) {
-            System.out.println(n + " - wrong range");
-            return;
-        }
-        PrimeNumbers table = new PrimeNumbers(n);
+		if (args.length!=0){
+			int n;
+			try {
+				n = Integer.parseInt(args[0]);
+			} catch (NumberFormatException ex) {
+				System.out.println(args[0] + " - not integer");
+				return;
+			}
+			if (n < 1) {	//if a number is not positive
+				System.out.println(n + " - wrong range");
+				return;
+			}
+			PrimeNumbers table = new PrimeNumbers(n);
 
-        for (int i = 1; i < args.length; i++)     //
-        {
-            try {
-                n = Integer.parseInt(args[i]);
-            } catch (NumberFormatException ex) {
-                System.out.println(args[i] + " - wrong data");
-                return;
-            }
-            if (n < 0 || n >= table.tab.length) {
-                System.out.println(n + " - number out of range");
-                continue;
-            } else {
-                System.out.println(n + " - " + table.number(n));
-            }
-        }
+			for (int i = 1; i < args.length; i++)	//for each paramether tries to return a prime number
+			{
+				try {
+					n = Integer.parseInt(args[i]);
+				} catch (NumberFormatException ex) {
+					System.out.println(args[i] + " - wrong data");
+					return;
+				}
+				if (n < 0 || n >= table.tab.length) {
+					System.out.println(n + " - number out of range");
+					continue;
+				} else {
+					System.out.println(n + " - " + table.number(n));
+				}
+			}
+		} else {
+			System.out.println("No arguments");
+		}
     }
 }
-
-/*
-
-}*/
