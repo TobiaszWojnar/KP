67e26d9718dda122eb8deeef8a4b046eb8afd6c7
TobiaszWojnar <wojnar.tobiasz@gmail.com>
2020-04-09 16:31:25 +0200

    list0

---
+++ kp 2020 java/src/l0/z2/l0z2.java
@@ -0,0 +1,15 @@
+package l0.z2;
+/**
+ * @author Tobiasz Wojnar
+ * Java Programing Course 2020
+ * list 0 task 2 - print arguments one per line
+ **/
+
+
+public class l0z2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++){
+            System.out.println(args[i]);
+        }
+    }
+}
